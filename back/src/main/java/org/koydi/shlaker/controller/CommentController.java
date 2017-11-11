8e10d7ec7f12757c7a336c3ec673c19c89dd84de
Alexey <alexey.simonchyk@gmail.com>
2017-11-12 00:30:20 +0300

    Some front end work

--- back/src/main/java/org/koydi/shlaker/controller/CommentController.java
+++ back/src/main/java/org/koydi/shlaker/controller/CommentController.java
@@ -1,10 +1,12 @@
 package org.koydi.shlaker.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/controller")
+@CrossOrigin("http://localhost:4200")
 public class CommentController {
 
 }
